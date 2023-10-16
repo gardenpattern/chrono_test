83d47e29e87debeb48e7c84f5f43b8e43e81cc2c
Garden <supe128@naver.com>
2023-10-16 12:12:28 +0900

    플레이어 기본 이동과 카메라 합동추가

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
